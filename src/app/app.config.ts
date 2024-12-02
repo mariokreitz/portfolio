290c5e7e526368c2a97547fa0ba421a74b5eecd3
Mario Kreitz <Mario.Kreitz@web.de>
2024-12-02 08:00:39 +0100

    feat: integrate ngx-translate into app configuration
    - Updated `app.config.ts` to configure internationalization with `@ngx-translate/core` and `@ngx-translate/http-loader`.
    - Added `HttpLoaderFactory` to load translation files from the `./i18n/` directory with a `.json` extension.
    - Configured `TranslateModule` with `defaultLanguage` set to 'de'.
    - Added required providers for `TranslateService` and `HttpClient` to the application configuration.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,31 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import {
+  TranslateLoader,
+  TranslateModule,
+  TranslateService,
+} from '@ngx-translate/core';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './i18n/', '.json');
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    TranslateService,
+    TranslateModule.forRoot({
+      defaultLanguage: 'de',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }).providers!,
+  ],
 };
