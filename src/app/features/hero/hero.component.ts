d8a32f833b88efa94ed11c688eb93475a032697e
Mario Kreitz <Mario.Kreitz@web.de>
2024-12-02 19:59:47 +0100

    feat: enhance hero component with lateral and scroller content
    - Imported and integrated `InfiniteScrollerComponent`, `LeftLateralComponent`, and `RightLateralComponent` into the hero component.
    - Added dynamic `scrollerContent` array for infinite scroller display.
    - Updated hero HTML to include `<app-left-lateral>`, `<app-right-lateral>`, and `<app-infinite-scroller>` components for enriched visual design and content presentation.

--- src/app/features/hero/hero.component.ts
+++ src/app/features/hero/hero.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
 import { TitleComponent } from './components/title/title.component';
+import { InfiniteScrollerComponent } from './components/infinite-scroller/infinite-scroller.component';
+import { LeftLateralComponent } from './components/left-lateral/left-lateral.component';
+import { RightLateralComponent } from './components/right-lateral/right-lateral.component';
 
 @Component({
   selector: 'app-hero',
-  imports: [TitleComponent],
+  imports: [
+    TitleComponent,
+    InfiniteScrollerComponent,
+    LeftLateralComponent,
+    RightLateralComponent,
+  ],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
 })
-export class HeroComponent {}
+export class HeroComponent {
+  scrollerContent = [
+    'Open to Work',
+    '•',
+    'Available for remote Work',
+    '•',
+    'Frontend Developer',
+    '•',
+    'Based in Stuttgart',
+    '•',
+    'Experienced with',
+    '•',
+    'HTML, CSS, JavaScript, Angular',
+    '•',
+  ];
+}
