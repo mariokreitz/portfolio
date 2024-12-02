fcd6952fbcb6ccd237b1c239168aaaa0674e2255
Mario Kreitz <Mario.Kreitz@web.de>
2024-12-02 13:35:55 +0100

    refactor: update imports and injection in HeaderComponent
    - Consolidated import statements for better readability.
    - Imported `TranslateModule` for translation functionality.
    - Replaced constructor-based injection with `inject` for the `TranslationService`.
    - Streamlined the component's code by ensuring proper service injection and module imports.

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslationService } from '../../services/translation.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  translate = inject(TranslationService);
 }
