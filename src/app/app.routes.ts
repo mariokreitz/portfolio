558caf884d28b385a6c097d208b8b931da437b07
Mario Kreitz <Mario.Kreitz@web.de>
2024-12-02 13:32:39 +0100

    feat: add route for HeroComponent in app.routes.ts
    - Imported `HeroComponent` from the `features/hero` folder.
    - Updated the `routes` array to include a default route (`path: ''`) that renders the `HeroComponent`.
    - Enabled navigation to the Hero section as the landing page of the application.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HeroComponent } from './features/hero/hero.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HeroComponent,
+  },
+];
