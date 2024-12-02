a8164dbc84f7131b8f12c1900865428eb151f81c
Mario Kreitz <Mario.Kreitz@web.de>
2024-12-02 08:01:13 +0100

    feat: integrate translation service into AppComponent
    - Updated `app.component.ts` to include `TranslationService` for managing language translations.
    - Added `TranslateModule` to the component imports for translation functionality.
    - Simplified dependency injection by using the `inject` function for `TranslationService`.
    - Refactored imports and component metadata for better clarity and maintainability.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslationService } from './service/translation.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslateModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'portfolio';
+
+  translate = inject(TranslationService);
 }
