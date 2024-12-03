26a8066d903b84525ca906bbdf98f6d62cc6699e
Mario Kreitz <Mario.Kreitz@web.de>
2024-12-03 15:45:22 +0100

    python Code kopieren feat: finalize about section component with image and who-am-i components
    - Completed the about section, integrating the image and who-am-i components as part of its content.

--- src/app/features/about/about.component.ts
+++ src/app/features/about/about.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { ImageComponent } from './components/image/image.component';
+import { WhoAmIComponent } from './components/who-am-i/who-am-i.component';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [ImageComponent, WhoAmIComponent],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
 })
-export class AboutComponent {
-
-}
+export class AboutComponent {}
