d1ab06e2a6600ebfc0d392d45979a28cda4d6fa7
Mario Kreitz <Mario.Kreitz@web.de>
2024-12-03 19:26:54 +0100

    feat: implemented single-skill component with hover effects and modal integration
    - Added structured HTML to `single-skill.component.html` to display individual skill icons with dynamic SVG image source and hover effects.
    - Integrated conditional rendering for modals: If `openModal` is true, the `app-modal` component is displayed.
    - Applied SCSS in `single-skill.component.scss` to style the skill cards:
      - Added hover effects with `bounce` and `scale` classes for interactive animations.
      - Implemented a bounce animation on hover using keyframes, as well as a scaling effect for enhanced user interaction.
      - Used flexbox layout for proper alignment of skill icon and name.
    - Integrated `@Input` in `single-skill.component.ts` to accept `skill` data as input, ensuring dynamic rendering of skill details.

--- src/app/features/skills/components/single-skill/single-skill.component.ts
+++ src/app/features/skills/components/single-skill/single-skill.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Skill } from '../../../../types/skill/skill';
+import { ModalComponent } from '../modal/modal.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-single-skill',
-  imports: [],
+  imports: [ModalComponent, CommonModule],
   templateUrl: './single-skill.component.html',
-  styleUrl: './single-skill.component.scss'
+  styleUrl: './single-skill.component.scss',
 })
 export class SingleSkillComponent {
-
+  @Input({ required: true }) skill!: Skill;
 }
