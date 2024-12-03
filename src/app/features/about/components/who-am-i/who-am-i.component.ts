1327cd3f70366ccebee5cc6ca1f77a2e5db7098f
Mario Kreitz <Mario.Kreitz@web.de>
2024-12-03 15:43:37 +0100

    feat: finalize who-am-i component with styles and translations
    - Completed the who-am-i component, including its styling.
    - Added translations for German (de) and English (en) to ensure multilingual support.

--- src/app/features/about/components/who-am-i/who-am-i.component.ts
+++ src/app/features/about/components/who-am-i/who-am-i.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslationService } from '../../../../core/services/translation.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-who-am-i',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './who-am-i.component.html',
-  styleUrl: './who-am-i.component.scss'
+  styleUrl: './who-am-i.component.scss',
 })
 export class WhoAmIComponent {
-
+  translate = inject(TranslationService);
 }
