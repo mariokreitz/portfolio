b3b4a2f6c48005da2745793cc3dda0c9ae238078
Mario Kreitz <Mario.Kreitz@web.de>
2024-12-03 12:21:26 +0100

    feat: add About section with components
    - Created a new `About` component in the `features` folder.
    - Added nested components `Image` and `WhoAmI` to structure the About section.

---
+++ src/app/features/about/components/who-am-i/who-am-i.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-who-am-i',
+  imports: [],
+  templateUrl: './who-am-i.component.html',
+  styleUrl: './who-am-i.component.scss'
+})
+export class WhoAmIComponent {
+
+}
