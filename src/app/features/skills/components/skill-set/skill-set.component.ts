820e529c232c4115589d2704d656a453d8ebf677
Mario Kreitz <Mario.Kreitz@web.de>
2024-12-03 19:24:44 +0100

    feat: implemented skill-set component
    - Updated `skill-set.component.html` to dynamically render `app-single-skill` components using an `*ngFor` loop.
    - Added responsive flexbox styling in `skill-set.component.scss` for wrapping and spacing skills, ensuring a clean layout.
    - Defined a `skills` array in `skill-set.component.ts` with detailed information for each skill, including names, SVG paths, effect classes, and modal triggers.

--- src/app/features/skills/components/skill-set/skill-set.component.ts
+++ src/app/features/skills/components/skill-set/skill-set.component.ts
@@ -1,11 +1,80 @@
 import { Component } from '@angular/core';
+import { SingleSkillComponent } from '../single-skill/single-skill.component';
+import { Skill } from '../../../../types/skill/skill';
 
 @Component({
   selector: 'app-skill-set',
-  imports: [],
+  imports: [SingleSkillComponent],
   templateUrl: './skill-set.component.html',
-  styleUrl: './skill-set.component.scss'
+  styleUrl: './skill-set.component.scss',
 })
 export class SkillSetComponent {
-
+  skills: Skill[] = [
+    {
+      name: 'HTML',
+      svg: 'HTML.svg',
+      effectClass: 'bounce',
+      openModal: false,
+    },
+    {
+      name: 'CSS',
+      svg: 'CSS.svg',
+      effectClass: 'bounce',
+      openModal: false,
+    },
+    {
+      name: 'JavaScript',
+      svg: 'JavaScript.svg',
+      effectClass: 'bounce',
+      openModal: false,
+    },
+    {
+      name: 'Material Design',
+      svg: 'Material Design.svg',
+      effectClass: 'bounce',
+      openModal: false,
+    },
+    {
+      name: 'TypeScript',
+      svg: 'TypeScript.svg',
+      effectClass: 'bounce',
+      openModal: false,
+    },
+    {
+      name: 'Angular',
+      svg: 'Angular.svg',
+      effectClass: 'bounce',
+      openModal: false,
+    },
+    {
+      name: 'Firebase',
+      svg: 'Firebase.svg',
+      effectClass: 'bounce',
+      openModal: false,
+    },
+    {
+      name: 'Git',
+      svg: 'Git.svg',
+      effectClass: 'bounce',
+      openModal: false,
+    },
+    {
+      name: 'Rest-Api',
+      svg: 'Rest-Api.svg',
+      effectClass: 'bounce',
+      openModal: false,
+    },
+    {
+      name: 'Scrum',
+      svg: 'Scrum.svg',
+      effectClass: 'bounce',
+      openModal: false,
+    },
+    {
+      name: 'Growth mindset',
+      svg: 'GrowthMindset.svg',
+      effectClass: 'scale',
+      openModal: true,
+    },
+  ];
 }
