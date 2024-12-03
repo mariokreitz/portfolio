b8808ab8238eeff761d97ecb8af3fada5553a87c
Mario Kreitz <Mario.Kreitz@web.de>
2024-12-03 19:25:44 +0100

    feat: implemented skill-box component with translations and styling
    - Added structured HTML layout to `skill-box.component.html`, incorporating a headline, subheadings, and a button for user interaction.
    - Integrated Angular Translate pipe for multilingual support, making headlines and content translatable (`en/de`).
    - Applied SCSS styling in `skill-box.component.scss`:
      - Used `contentBox` mixin for consistent box styling.
      - Designed typography for headlines, subheadings, and content, emphasizing the green highlight for keywords.
      - Configured responsive flexbox layout for alignment and spacing.
    - Integrated translation service in `skill-box.component.ts` to handle dynamic content switching based on selected language.

--- src/app/features/skills/components/skill-box/skill-box.component.ts
+++ src/app/features/skills/components/skill-box/skill-box.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslationService } from '../../../../core/services/translation.service';
+import { TranslateModule } from '@ngx-translate/core';
+import { ButtonComponent } from '../../../../shared/components/button/button.component';
 
 @Component({
   selector: 'app-skill-box',
-  imports: [],
+  imports: [TranslateModule, ButtonComponent],
   templateUrl: './skill-box.component.html',
-  styleUrl: './skill-box.component.scss'
+  styleUrl: './skill-box.component.scss',
 })
 export class SkillBoxComponent {
-
+  translate = inject(TranslationService);
 }
