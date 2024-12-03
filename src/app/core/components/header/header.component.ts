310f141271dcd57003a169c78879db9d6573b564
Mario Kreitz <Mario.Kreitz@web.de>
2024-12-03 09:13:47 +0100

    feat: complete language switcher functionality and styling
    - Implemented the language switcher in the header component with full functionality.
    - Added styling for smooth integration into the header layout.
    - Enabled translation between German (de) and English (en) languages.

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
 import { TranslationService } from '../../services/translation.service';
 import { TranslateModule } from '@ngx-translate/core';
 
@@ -8,6 +8,29 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
+
+/**
+ * HeaderComponent is a component that renders the header section of the application.
+ * It contains the logo and a language switcher.
+ *
+ * @example
+ * <app-header></app-header>
+ */
 export class HeaderComponent {
   translate = inject(TranslationService);
+
+  @HostListener('change', ['$event.target'])
+  /**
+   * Handles the change event of the language switcher input element.
+   * @param inputElement The HTMLInputElement that triggered this event.
+   * Switches the language of the application based on whether the input element is checked or not.
+   * When the input element is checked, the language is switched to German.
+   * When the input element is not checked, the language is switched to English.
+   */
+  onInputChange(inputElement: HTMLInputElement): void {
+    const isChecked = inputElement.checked as boolean;
+
+    if (!isChecked) this.translate.switchLanguage('en');
+    else this.translate.switchLanguage('de');
+  }
 }
