0a7c230e7590bb9f5dfa504ae183b386dfd0d6e5
Mario Kreitz <Mario.Kreitz@web.de>
2024-12-03 19:23:41 +0100

    feat: structure and style skills section
    - Replaced boilerplate code in `skill.component.html` with a structured layout including `<app-skill-box>` and `<app-skill-set>` components.
    - Added styling to `skill.component.scss` for the skills section, utilizing `$gradient-skill` for the background and a flexible layout for `.content`.

--- src/app/features/skills/skills.component.ts
+++ src/app/features/skills/skills.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { SkillBoxComponent } from './components/skill-box/skill-box.component';
+import { SkillSetComponent } from './components/skill-set/skill-set.component';
 
 @Component({
   selector: 'app-skills',
-  imports: [],
+  imports: [SkillBoxComponent, SkillSetComponent],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.scss'
+  styleUrl: './skills.component.scss',
 })
-export class SkillsComponent {
-
-}
+export class SkillsComponent {}
