2696d6ad97f34b0c2005d85bd7903b86f24c26d5
Mario Kreitz <Mario.Kreitz@web.de>
2024-12-03 19:21:17 +0100

    feat: include skills section in home component
    - Added `<app-skills></app-skills>` in the home component HTML to display the newly created skills section.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../../features/hero/hero.component';
 import { AboutComponent } from '../../features/about/about.component';
+import { SkillsComponent } from '../../features/skills/skills.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent, AboutComponent],
+  imports: [HeroComponent, AboutComponent, SkillsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
