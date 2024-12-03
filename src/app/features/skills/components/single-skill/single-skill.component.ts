8a3bbb29de833e4a62215cf86cf37dab45c9eccf
Mario Kreitz <Mario.Kreitz@web.de>
2024-12-03 15:55:20 +0100

    feat: add components for skills section
    - Created three new components for the "Skills" section: single-skill, skill-box, and skill-set.
    - Components set up for further development and styling.

---
+++ src/app/features/skills/components/single-skill/single-skill.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-skill',
+  imports: [],
+  templateUrl: './single-skill.component.html',
+  styleUrl: './single-skill.component.scss'
+})
+export class SingleSkillComponent {
+
+}
---
+++ src/app/features/skills/components/skill-box/skill-box.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skill-box',
+  imports: [],
+  templateUrl: './skill-box.component.html',
+  styleUrl: './skill-box.component.scss'
+})
+export class SkillBoxComponent {
+
+}
