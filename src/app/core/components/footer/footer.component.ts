6cb270d9091d4ecabfcf8cf1c88ca37a3831bb67
Mario Kreitz <Mario.Kreitz@web.de>
2024-12-04 19:31:50 +0100

    feat: create footer with logo, copyright, and links
    - Added footer to the layout with logo aligned to the right.
    - Included copyright and name in the center.
    - Added links to social media, email (mailto:), and important legal notices (sidemaps).

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { NavLink } from '../../../types/link/link';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-
+  links: NavLink[] = [
+    {
+      name: 'Github',
+      url: 'https://github.com/mariokreitz',
+    },
+    {
+      name: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/mario-kreitz/',
+    },
+    {
+      name: 'Email',
+      url: 'contact@mario-kreitz.dev',
+      email: true,
+    },
+    {
+      name: 'Legal Notice',
+      url: 'legal',
+      sidemap: true,
+    },
+    {
+      name: 'Imprint',
+      url: 'imprint',
+      sidemap: true,
+    },
+  ];
 }
