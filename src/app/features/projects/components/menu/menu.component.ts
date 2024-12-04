e0f9864ca221216b5d8d85f3f047f9892b2da8f1
Mario Kreitz <Mario.Kreitz@web.de>
2024-12-04 19:35:08 +0100

    feat: update project descriptions and add modal handling functions
    - Replaced the old `description` with `descriptionEN` and `descriptionDE` in project objects for both English and German descriptions.
    - Implemented `openModal`, `closeModal`, and `nextProject` methods for handling modal actions.
    - The `openModal` method triggers the `ModalService` to display the selected project in the modal.
    - The `closeModal` method clears the modal content by setting the modal data to `null`.
    - The `nextProject` method updates the modal content to the next project based on the given `id`, with wrapping behavior when reaching the last project.

--- src/app/features/projects/components/menu/menu.component.ts
+++ src/app/features/projects/components/menu/menu.component.ts
@@ -1,23 +1,33 @@
 import { Component, Input } from '@angular/core';
 import { Project } from '../../../../types/projects/project';
 import { MenuItemComponent } from './menu-item/menu-item.component';
+import { MenuModalComponent } from './menu-modal/menu-modal.component';
+import { ModalService } from '../../services/modal.service';
 
 @Component({
   selector: 'app-menu',
-  imports: [MenuItemComponent],
+  imports: [MenuItemComponent, MenuModalComponent],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss',
 })
 export class MenuComponent {
   @Input() projects: Project[] = [];
 
-  constructor() {
+  modalData: Project | null = null;
+
+  constructor(private modalService: ModalService) {
+    this.modalService.modalData$.subscribe((data) => {
+      this.modalData = data;
+    });
+
     this.projects = [
       {
-        id: 0,
+        id: 1,
         name: 'Join',
-        description:
-          'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
+        descriptionEN:
+          'JOIN is a task manager inspired by the Kanban system, designed to track your teams tasks, responsibilities, and progress. With its intuitive interface, JOIN makes collaboration and task management simple.',
+        descriptionDE:
+          'Aufgabenmanager inspiriert vom Kanban-System. JOIN ist ein benutzerfreundliches und leistungsstarkes Tool, das dir hilft, die Aufgaben, Verantwortlichkeiten und den Fortschritt deines Teams zu verfolgen.',
         screenshotUrl:
           'https://raw.githubusercontent.com/mariokreitz/JOIN/refs/heads/main/screenshot.png',
         tech: [
@@ -42,10 +52,12 @@ export class MenuComponent {
         demoUrl: 'https://join.mario-kreitz.dev/',
       },
       {
-        id: 1,
+        id: 2,
         name: 'Shadowhound',
-        description:
-          'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
+        descriptionEN:
+          'A side-scrolling jump-and-run game where players guide Shadowhound, a small black dog, through a shadowy forest, defeating monsters, earning points, and navigating a dark, minimalist world in black, gray, and white. ',
+        descriptionDE:
+          'Ein Side-Scrolling-Spiel, bei dem die Spieler einen schwarzen Hund, durch einen Wald steuern, Monster besiegen, Punkte sammeln und eine minimalistische Welt in Schwarz, Grau und Weiß erkunden.',
         screenshotUrl:
           'https://raw.githubusercontent.com/mariokreitz/Shadowhound/refs/heads/main/screenshot_menu.png',
         tech: [
@@ -66,10 +78,12 @@ export class MenuComponent {
         demoUrl: 'https://shadowhound.mario-kreitz.dev/',
       },
       {
-        id: 2,
+        id: 3,
         name: 'Pokédex',
-        description:
-          'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
+        descriptionEN:
+          'A digital encyclopedia of Pokémon, providing detailed information on each species, including their abilities, types, items, and sounds. Explore and discover the world of Pokémon with ease! ',
+        descriptionDE:
+          'Ein digitales Pokémon-Enzyklopädie, die detaillierte Informationen zu jeder Spezies bietet, einschließlich ihrer Fähigkeiten, Typen, Items und Geräusche. Entdecke die Welt der Pokémon mit Leichtigkeit!',
         screenshotUrl:
           'https://raw.githubusercontent.com/mariokreitz/pokedex-v2/refs/heads/main/src/assets/screenshot.png',
         tech: [
@@ -86,5 +100,32 @@ export class MenuComponent {
         demoUrl: 'https://pokedex.mario-kreitz.dev/',
       },
     ];
+  }
+
+  /**
+   * Opens the modal with the given project as content.
+   * Utilizes the ModalService to update the modal state.
+   * @param project The project to be displayed in the modal.
+   */
+  openModal(project: Project) {
+    this.modalService.openModal(project);
+  }
+
+  /**
+   * Closes the currently open modal by setting the modal data to null.
+   * Utilizes the ModalService to update the modal state.
+   */
+  closeModal() {
+    this.modalService.closeModal();
+  }
+
+  /**
+   * Updates the modal data to the project with the given id.
+   * If the id is equal to the number of projects, it wraps around to the first project.
+   * @param id The id of the project to be displayed next.
+   */
+  nextProject(id: number) {
+    const index = id === this.projects.length ? 0 : id;
+    this.modalData = this.projects[index];
   }
 }
