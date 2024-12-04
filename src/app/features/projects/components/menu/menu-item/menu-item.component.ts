0ecef77dec5ea28d4403a788d1f8f335596f37fe
Mario Kreitz <Mario.Kreitz@web.de>
2024-12-04 12:15:36 +0100

    feat: updated menu-item component to display project data
    - Modified the `menu-item` component to render project data passed from the parent `menu` component, which holds an array of objects related to the projects.

--- src/app/features/projects/components/menu/menu-item/menu-item.component.ts
+++ src/app/features/projects/components/menu/menu-item/menu-item.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Project } from '../../../../../types/projects/project';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu-item',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './menu-item.component.html',
-  styleUrl: './menu-item.component.scss'
+  styleUrl: './menu-item.component.scss',
 })
 export class MenuItemComponent {
-
+  @Input() project!: Project;
+  @Input() isLast!: boolean;
 }
