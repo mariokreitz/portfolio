26c2af4f89b9c8d4fbf79e550084c279a8fd5697
Mario Kreitz <Mario.Kreitz@web.de>
2024-12-11 18:26:07 +0100

    feat: Add route for privacy policy page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,16 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ImprintComponent } from './pages/imprint/imprint.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { PrivatPolicyComponent } from './pages/privat-policy/privat-policy.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'policy',
+    component: PrivatPolicyComponent,
   },
   {
     path: 'imprint',
