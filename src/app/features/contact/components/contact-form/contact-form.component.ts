a276fb4bfdf1bc569cb56f61442f01ee18955db9
Mario Kreitz <Mario.Kreitz@web.de>
2024-12-11 18:30:12 +0100

    feat: Create  form components in contact section

--- src/app/features/contact/components/contact-form/contact-form.component.ts
+++ src/app/features/contact/components/contact-form/contact-form.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../../shared/components/button/button.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-contact-form',
-  imports: [],
+  imports: [ButtonComponent, RouterModule],
   templateUrl: './contact-form.component.html',
-  styleUrl: './contact-form.component.scss'
+  styleUrl: './contact-form.component.scss',
 })
-export class ContactFormComponent {
-
-}
+export class ContactFormComponent {}
