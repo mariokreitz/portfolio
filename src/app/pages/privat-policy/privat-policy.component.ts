3ac0a1df0332918a21259417d871c3d23ba0c865
Mario Kreitz <Mario.Kreitz@web.de>
2024-12-11 18:26:50 +0100

    feat: Create privacy-policy component page

---
+++ src/app/pages/privat-policy/privat-policy.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-privat-policy',
+  imports: [],
+  templateUrl: './privat-policy.component.html',
+  styleUrl: './privat-policy.component.scss'
+})
+export class PrivatPolicyComponent {
+
+}
